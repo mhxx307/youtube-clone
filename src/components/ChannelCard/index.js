b2a9b29eb7e7e65bdc118aa318e89375af678dec
mhxx307 <minhquan.lavo@gmail.com>
2022-12-04 18:56:14 +0700

    render channel ui

--- src/components/ChannelCard/index.js
+++ src/components/ChannelCard/index.js
@@ -1,5 +1,61 @@
-function ChannelCard() {
-    return <div>ChannelCard</div>;
+import { CheckCircle } from '@mui/icons-material';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import images from '~/assets/imgs';
+
+function ChannelCard({ channelDetail }) {
+    const channelTitle = channelDetail?.snippet?.title;
+    const channelThumbnail = channelDetail?.snippet?.thumbnails?.high?.url;
+    const subCount = channelDetail?.statistics?.subscriberCount;
+    return (
+        <Box
+            sx={{
+                boxShadow: 'none',
+                borderRadius: '20px',
+                display: channelDetail?.id?.channelId ? 'flex' : 'none',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: { xs: '356px', md: '320px' },
+                height: '326px',
+                margin: 'auto',
+            }}
+        >
+            <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+                <CardContent
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        textAlign: 'center',
+                        color: '#fff',
+                    }}
+                >
+                    <CardMedia
+                        image={channelThumbnail || images.logo}
+                        alt={channelTitle}
+                        sx={{
+                            borderRadius: '50%',
+                            height: '180px',
+                            width: '180px',
+                            mb: 2,
+                            border: '1px solid #e3e3e3',
+                        }}
+                    />
+                    <Typography variant="h6">
+                        {channelTitle}
+                        <CheckCircle
+                            sx={{ fontSize: 14, color: 'gray', ml: '5px' }}
+                        />
+                    </Typography>
+                    {subCount && (
+                        <Typography variant="subtitle2">
+                            {parseInt(subCount).toLocaleString()} Subscribers
+                        </Typography>
+                    )}
+                </CardContent>
+            </Link>
+        </Box>
+    );
 }
 
 export default ChannelCard;
--- src/components/Videos/index.js
+++ src/components/Videos/index.js
@@ -7,7 +7,7 @@ function Videos({ videos }) {
         <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
             {videos.map((item, index) => {
                 return (
-                    <Box>
+                    <Box key={index}>
                         {item.id.videoId && <VideoCard video={item} />}
                         {item.id.channelId && (
                             <ChannelCard channelDetail={item} />
